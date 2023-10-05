1056e72540082f04fcf6d9444fde4173a5e9c71a
Nathaly Posada <99698453+Nathalyposada07@users.noreply.github.com>
2023-10-04 22:39:53 -0500

    pokedex ok

---
+++ src/components/Home.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { getUsername } from '../store/slices/userName.slice';
+
+
+const Home = () => {
+    const [name, setName] = useState("");
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const goPokedex = () => {
+        dispatch(getUsername(name));
+        navigate("/pokedex")
+    }
+
+    return (
+        <div className='home'>
+            <div className='container__home'>
+                <img className='logo__home' src="logopokemon.png" alt="logo" />
+                <p className='tittle__home'><strong>Pokedex </strong></p>
+                <div class="nes-field">
+                    <input placeholder='Your name' className='nes-input ' type="text" value={name} onChange={e => setName(e.target.value)} />
+                </div>
+                <br />
+                <button className='nes-btn is-success' onClick={() => goPokedex()}>Go</button>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
